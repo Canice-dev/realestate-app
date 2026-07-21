f74b76ea55c13f8b076cbe562fc05daee350eae6
Canicedev <canice.aba.dev@gmail.com>
2026-07-21 20:39:29 +0100

    feat: update dependencies and add Tailwind CSS configuration Added @clerk/expo, babel-preset-expo, expo-secure-store, nativewind, and prettier-plugin-tailwindcss to dependencies.

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import "../global.css";
+
+export default function Index() {
+  return (
+    <SafeAreaView>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </SafeAreaView>
+  );
+}
